8e8f25e9eee649b6630923ca2783700450337ec8
Compte_de_Service WEBSERVICES <webservices@webservices2017.ipsl.upmc.fr>
2017-06-06 16:50:24 +0200

    Scan work (dods/public)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 simuFinder
 
-free pattern search for simulations run at IPSL
+Tags and free pattern search for simulations run at IPSL based on select2 (https://select2.github.io)
 
 <img src="https://github.com/PBrockmann/simuFinder/raw/master/screenshot_01.png" width="75%" />
--- create_list.bash
+++ create_list.bash
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-server='https://vesg.ipsl.upmc.fr/thredds/catalog/work_thredds'
-
+#------------------------------------------------------------------
 echo 'var input = `'
 
-for dir in `ls -d1 /ccc/work/cont003/thredds/p86caub/*/*/*/*/MONITORING`; do
-#for dir in `ls -d1 /ccc/work/cont003/thredds/*/*/*/*/*/MONITORING`; do
-
-	nb=`ls $dir/files | wc -l`
-	if [[ $nb -ne 0 ]] ; then
-		base=`echo $dir | cut -d/ -f 6-`
-		echo $server/$base
+#------------------------------------------------------------------
+server='https://vesg.ipsl.upmc.fr/thredds/catalog/work_thredds'
+for dir in `ls -d1 /ccc/work/cont003/thredds/*/*/*/*/*/MONITORING`; do
+	if [ -d $dir/files ] ; then
+		nb=`ls $dir/files/*.nc | wc -l`
+		if [[ $nb -ne 0 ]] ; then
+			base=`echo $dir | cut -d/ -f 6- | sed -e 's%/MONITORING%%'`
+			echo $server/$base
+		fi
 	fi
-
 done
 
+#------------------------------------------------------------------
+server='https://vesg.ipsl.upmc.fr/thredds/catalog/work'
+for dir in `ls -d1 /ccc/work/cont003/dods/public/*/*/*/*/MONITORING`; do
+	if [ -d $dir/files ] ; then
+		nb=`ls $dir/files/*.nc | wc -l`
+		if [[ $nb -ne 0 ]] ; then
+			base=`echo $dir | cut -d/ -f 7- | sed -e 's%/MONITORING%%'`
+			echo $server/$base
+		fi
+	fi
+done
+
+#------------------------------------------------------------------
 echo '`;'
+
