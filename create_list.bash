28485293508270cc72f18af476292d3a1644da17
Compte_de_Service WEBSERVICES <webservices@webservices2017.ipsl.upmc.fr>
2017-06-07 14:47:10 +0200

    Scan dods/public

--- create_list.bash
+++ create_list.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+
 #------------------------------------------------------------------
 echo 'var input = `'
 
 #------------------------------------------------------------------
 server='https://vesg.ipsl.upmc.fr/thredds/catalog/work_thredds'
+
+# DRS with 4 levels after login
 for dir in `ls -d1 /ccc/work/cont003/thredds/*/*/*/*/*/MONITORING`; do
 	if [ -d $dir/files ] ; then
-		nb=`ls $dir/files/*.nc | wc -l`
+		nb=`ls $dir/files/*.nc 2>/dev/null | wc -l`
 		if [[ $nb -ne 0 ]] ; then
 			base=`echo $dir | cut -d/ -f 6- | sed -e 's%/MONITORING%%'`
 			echo $server/$base
@@ -17,9 +20,22 @@ done
 
 #------------------------------------------------------------------
 server='https://vesg.ipsl.upmc.fr/thredds/catalog/work'
+
+# DRS with 3 levels after login
 for dir in `ls -d1 /ccc/work/cont003/dods/public/*/*/*/*/MONITORING`; do
 	if [ -d $dir/files ] ; then
-		nb=`ls $dir/files/*.nc | wc -l`
+		nb=`ls $dir/files/*.nc 2>/dev/null | wc -l`
+		if [[ $nb -ne 0 ]] ; then
+			base=`echo $dir | cut -d/ -f 6- | sed -e 's%/MONITORING%%'`
+			echo $server/$base
+		fi
+	fi
+done
+
+# DRS with 4 levels after login
+for dir in `ls -d1 /ccc/work/cont003/dods/public/*/*/*/*/*/MONITORING`; do
+	if [ -d $dir/files ] ; then
+		nb=`ls $dir/files/*.nc 2>/dev/null | wc -l`
 		if [[ $nb -ne 0 ]] ; then
 			base=`echo $dir | cut -d/ -f 7- | sed -e 's%/MONITORING%%'`
 			echo $server/$base
@@ -29,4 +45,3 @@ done
 
 #------------------------------------------------------------------
 echo '`;'
-
